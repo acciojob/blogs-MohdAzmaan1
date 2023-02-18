f34942b622f3629ed4c74b67b8f49e2c10e2d13b
Mohd Azmaan <mohdazmaan3@gmail.com>
2023-02-18 19:05:11 +0530

    Check 7

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -1,11 +1,17 @@
 package com.driver.services;
 
 import com.driver.models.Blog;
+import com.driver.models.Image;
 import com.driver.models.User;
 import com.driver.repositories.BlogRepository;
+import com.driver.repositories.ImageRepository;
 import com.driver.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class BlogService {
@@ -15,12 +21,13 @@ public class BlogService {
     @Autowired
     UserRepository userRepository;
 
-    public void createAndReturnBlog(Integer userId, String title, String content) {
+    public Blog createAndReturnBlog(Integer userId, String title, String content) {
         //create a blog at the current time
         User user = userRepository.findById(userId).get();
         Blog blog = new Blog(title,content,user);
         userRepository.save(user);
         user.getBlogList().add(blog);
+        return blog;
     }
 
     public void deleteBlog(int blogId){
